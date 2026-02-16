df4885f66a23e8720488375f95afbf7377f21916
naumanshah1 <140418835+naumanshah1@users.noreply.github.com>
2026-02-16 09:46:45 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -101,5 +101,5 @@ def serve(path):
 # RUN LOCALLY
 # --------------------------------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
